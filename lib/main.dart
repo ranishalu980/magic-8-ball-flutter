f9e8090f6e58acb626c986749bc1edd7fd758ade
Shalu Rani <90301799+Ranishalu224@users.noreply.github.com>
2021-09-21 17:17:38 +0530

    feat:Assignment#2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+import 'package:flutter/rendering.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.lightBlueAccent),
+        body: BallPage(),
       ),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+  void randomBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(children: [
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton(
+              onPressed: () {
+                randomBall();
+              },
+              child: Image.asset('images/ball$ballNumber.png')),
+        )),
+      ]),
     );
+  }
+}
